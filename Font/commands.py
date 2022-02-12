3996538538c7d61e12cc9020b5bea07a84f7f6c7
αвιנтн <92922942+DareAbijth@users.noreply.github.com>
2022-02-12 15:35:45 +0530

    Update commands.py

--- Font/commands.py
+++ Font/commands.py
@@ -1,4 +1,17 @@
-await m.reply_text(
+import os
+from config import Config
+from .fonts import Fonts
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(filters.command('start'))
+async def start(c, m):
+    owner = await c.get_users(int(Config.OWNER_ID))
+    owner_username = owner.username if owner.username else 'No1_Bots'
+
+    
+    await m.reply_text(
         text=text,
         reply_markup=InlineKeyboardMarkup(buttons)
     )
